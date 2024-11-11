dda0949b79491e5c3a979f7a9a324b86ee876aef
Anas Baig <anasraees33@gmail.com>
2024-11-11 14:49:23 +0500

    donee

--- src/app/(pages)/skills/page.js
+++ src/app/(pages)/skills/page.js
@@ -6,7 +6,7 @@ const Skills = forwardRef((props, ref) => {
   return (
     <>
       <div ref={ref}
-        className={`boxes-skill relative w-full py-16  bg-[#0f0d0d] dark:bg-white  border-b-4 font-bold flex flex-col gap-8`}
+        className={`boxes-skill relative w-full py-16  bg-[#0f0d0d] dark:bg-gray-100  border-b-4 font-bold flex flex-col gap-8`}
       >
         <h1
           className={`text-center font-semibold font-Noto text-white dark:text-heading   md:text-[50px] sm:text-[50px] text-[40px]   lg:text-[50px]  `}
@@ -20,7 +20,7 @@ const Skills = forwardRef((props, ref) => {
               <div
                 key={index}
                 // Store reference to each box
-                className=" box2 sm:justify-center justify-center py-4 px-4 bg-gray-50 shadow-custom  shadow-gray-500 md:m-4  mt-6 rounded-lg flex items-center hover:scale-125 cursor-pointer duration-500"
+                className=" box2 sm:justify-center justify-center py-4 px-4 bg-gray-50 shadow-md dark:shadow-lg   shadow-gray-500 md:m-4  mt-6 rounded-lg flex items-center hover:scale-125 cursor-pointer duration-500"
               >
                 <img alt="" src={el.link} className="sm:w-12 w-8 " />
                 <h4 className=" text-[16px] xl:text-[23px]  sm:text-[20px] ml-3 sm:ml-4">{el.name}</h4>
